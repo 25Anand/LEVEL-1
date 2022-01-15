c43068b8646ee6422d11bedab88616b21a4d8806
25Anand <72190850+25Anand@users.noreply.github.com>
2022-01-16 02:08:54 +0530

    Create Pattern2.java

---
+++ basic/Pattern2.java
@@ -0,0 +1,17 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class pattern2 {
+    public static void main(String arg[]) {
+        System.out.println("enter the value for which pattern is to be printed"); 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n; j++) {
+                System.out.print(" " + (n-j+1);
+             }
+            System.out.println();
+        }
+    }
+}
