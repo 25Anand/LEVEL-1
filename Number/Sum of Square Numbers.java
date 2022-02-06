6fa4196f8d54ce58d1dab95bb8a35f56e7205103
BHARAT BHUSHAN ANAND <72190850+25Anand@users.noreply.github.com>
2022-02-07 00:47:30 +0530

    problem_633

---
+++ Number/Sum of Square Numbers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        // if(c<0){
+        //      return false;
+        // }
+        int a=0, b=(int)(Math.sqrt(c));
+        while(a<=b)
+        {
+            int sum=(a*a)+(b*b);
+            if(sum<0)
+            {
+                b--;
+            }
+            if(sum == c)
+            {
+                return true;
+            }
+            if(sum < c)
+            {
+                a++;
+            }
+             else{
+                b--; 
+            }
+        }
+        return false;
+    } 
+}
