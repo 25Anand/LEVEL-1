e06d0083854ced714a9307a297874ea203a89ff9
BHARAT BHUSHAN ANAND <72190850+25Anand@users.noreply.github.com>
2022-02-19 23:59:05 +0530

    Problem_367

---
+++ Number/Valid Perfect Square.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+         long t=num;
+        while(t*t>num){
+            t=(t+num/t)/2;
+        }
+        return t*t==num;
+    }
+}
